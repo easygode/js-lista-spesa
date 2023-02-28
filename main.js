49178476ba58fd95455c7636ac70300c6b884492
easygode <bimalbenvenuti@gmail.com>
2023-02-28 18:39:38 +0100

    JS ciclo while

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const shoppingList = [
+    'Milk',
+    'Rice',
+    'Noodles',
+    'Biscuits',
+    'Besciamella',
+    'Flour',
+    'Cubes',
+    'Sugar'
+]
+
+const list = document.querySelector('.shopping-list');
+
+
+let i = 0;
+while (i < shoppingList.length) {
+    const li = document.createElement("li");
+    li.append(shoppingList[i]);
+    list.append(li);
+    i++;
+}
